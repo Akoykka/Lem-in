729a28549b11c51c740976af8d517407976b9951
Atte Koykka <akoykka@c1r6p12.hive.fi>
2022-09-20 14:12:43 +0300

    onestar hash repairedd

--- hash/hash.c
+++ hash/hash.c
@@ -6,7 +6,7 @@
 /*   By: akoykka <akoykka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 17:11:10 by akoykka           #+#    #+#             */
-/*   Updated: 2022/09/20 12:15:04 by akoykka          ###   ########.fr       */
+/*   Updated: 2022/09/20 14:12:03 by akoykka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,7 +85,7 @@ void hash_init(unsigned int table_size)
 
 	temp = hash_storage();
 	temp->table_size = table_size;
-	temp->table = (t_hash **)ft_memalloc(sizeof(t_hash *) * table_size);
+	temp->table = (t_hash *)ft_memalloc(sizeof(t_hash) * table_size);
 	if (!temp->table)
 		exit(1);
 }
@@ -94,18 +94,16 @@ void hash_destroy(void)
 {
 	t_table *temp;
 	unsigned long i;
-	t_hash **hashtable;
+	t_hash *hashtable;
 	i = 0;
 	temp = hash_storage();
 	hashtable = temp->table;
 	while (temp->table_size > i)
 	{
-		if (hashtable[i])
+		if (hashtable[i].name)
 		{
-			free(hashtable[i]->name);
-			hashtable[i]->name = NULL;
-			free(hashtable[i]);
-			hashtable[i] = NULL;
+			free(hashtable[i].name);
+			hashtable[i].name = NULL;
 		}
 		++i;
 	}
@@ -115,7 +113,7 @@ t_hash *hash_get(char *name)
 {
 	t_table *temp;
 	unsigned long digest;
-	t_hash **hash_table;
+	t_hash *hash_table;
 	if(!name)
 		return NULL;
 
@@ -123,10 +121,10 @@ t_hash *hash_get(char *name)
 	digest = hash_djb2(name) % temp->table_size;
 	hash_table = temp->table;
 
-	while (hash_table[digest])
+	while (hash_table[digest].name)
 	{
-		if (!ft_strcmp(hash_table[digest]->name, name))
-			return (hash_table[digest]);
+		if (!ft_strcmp(hash_table[digest].name, name))
+			return(&hash_table[digest]);
 		digest++;
 		if (temp->table_size == digest)
 			digest = 0;
@@ -138,42 +136,29 @@ t_hash *hash_get(char *name)
 	exit(1);
 }
 
-t_hash *hash_new(char *name, int x, int y, int number)
-{
-	t_hash *new;
-	
-	new = (t_hash *)ft_memalloc(sizeof(t_hash));
-	if(!new)
-		exit(1);
-	new->name = ft_strdup(name);
-	if (!new->name)
-		exit(1);
-	new->xy[0] = x;
-	new->xy[1] = y;
-	new->number = number;
-
-	return (new);
-}
-
 void hash_add(char *name, int x, int y, int number)
 {
 	t_table *temp;
 	unsigned long digest;
-	t_hash **hash_table;
+	t_hash *hash_table;
 	if(!name)
 		return ;
-
 	temp = hash_storage();
 	digest = hash_djb2(name) % temp->table_size;
 	hash_table = temp->table;
 
-	while (hash_table[digest])
+	while (hash_table[digest].name)
 	{
 		digest++;
 		if (temp->table_size == digest)
 			digest = 0;
 	}
-	hash_table[digest] = hash_new(name, x, y, number);
+	hash_table[digest].name = ft_strdup(name);
+	if(hash_table[digest].name)
+		exit(1);
+	hash_table[digest].xy[0] = x;
+	hash_table[digest].xy[1] = y;
+	hash_table[digest].number = number;
 }	
 
 
